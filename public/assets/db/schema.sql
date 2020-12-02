8ceec05a0b73fded0d74ba2ff97fdfee9327c685
Tommy Willen <tommyallen1215@gmail.com>
2020-12-02 09:41:31 -0800

    changed file arrangement and added initial handlebars setup (without braces)

---
+++ public/assets/db/schema.sql
@@ -0,0 +1,12 @@
+-- this first line is only used in the dev stage. you will want to remove it once your db is deployed.
+DROP DATABASE IF EXISTS burger_timeDB;
+CREATE database burger_timeDB;
+
+USE burger_timeDB;
+-- this is the table that will store burger name and if it has been eaten
+CREATE TABLE burger (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(144),
+    is_eaten boolean default false,
+    PRIMARY KEY(id)
+);
--- server.js
+++ server.js
@@ -1,16 +1,50 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const mysql = require("mysql");
+require("dotenv").config();
+var exphbs = require("express-handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));``
+app.use(express.static("public"));
 
-// utilizes the two routes modules in the routes directory
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Port is defined in .env file
+    port: process.env.DB_PORT,
+  
+    // Your username (defined in .env file)
+    user: process.env.DB_USER,
+  
+    // Your password (defined in .env file)
+    password: process.env.DB_PASSWORD,
+    // You will need to create the db and add it here or in a .env file
+    database: process.env.DB_DB
+  });
+  // connects to database
+  connection.connect( (err) => {
+    if (err) throw err;
+
+  });
+
+app.get("*", (req,res) => {
+    connection.query("SELECT * FROM burger;", (err, data) => {
+        if (err) throw err;
+
+        let unEaten = data.filter(burger => !burger.is_eaten);
+        let eaten = data.filter(burger => burger.is_eaten);
+        res.render("index", { unEaten: unEaten, eaten: eaten });
+    });
+});
+
 
 // starts the server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
   });
